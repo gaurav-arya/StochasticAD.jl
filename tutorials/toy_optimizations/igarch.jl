9f5597cd43aa4d468abac79389dbf44c9c3783fd
Moritz Schauer <moritzschauer@web.de>
2022-10-19 20:26:25 +0200

    Whitespace

--- tutorials/toy_optimizations/igarch.jl
+++ tutorials/toy_optimizations/igarch.jl
@@ -30,8 +30,8 @@ _, z_obs = igarch(a, b, c, n, λ0) # 140 in first run
 # Posterior estimate of parameter p=λ0 given z_obs=140 (assume we don't know)
 function X(p, z_obs = 140, n = 10)
     a, b, c = [0.25, 0.9, 0.51]
-    λ, _ = igarch(a, b, c, n - 1, p) 
-    logpdf(Exponential(100.0), p)  + logpdf(Poisson(λ), z_obs) ## prior + likelihood
+    λ, _ = igarch(a, b, c, n - 1, p)
+    logpdf(Exponential(100.0), p) + logpdf(Poisson(λ), z_obs) ## prior + likelihood
 end
 
 # Maximize likelihood Adam and Optimize

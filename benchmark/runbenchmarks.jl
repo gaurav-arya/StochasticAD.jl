2b772af2ad2c6ee37f155b42bad64074a5e9cee5
Gaurav Arya <gauravarya272@gmail.com>
2023-01-30 21:29:58 +0800

    Show benchmarking results

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -1,5 +1,6 @@
 using PkgBenchmark
-benchmarkpkg(dirname(@__DIR__),
-             BenchmarkConfig(env = Dict("JULIA_NUM_THREADS" => "1",
-                                        "OMP_NUM_THREADS" => "1")),
-             resultfile = joinpath(@__DIR__, "result.json"))
+results = benchmarkpkg(dirname(@__DIR__),
+                       BenchmarkConfig(env = Dict("JULIA_NUM_THREADS" => "1",
+                                                  "OMP_NUM_THREADS" => "1")),
+                       resultfile = joinpath(@__DIR__, "result.json"))
+@show results

deba484926a1896ea6db1ed685492aff12fff6e2
Gaurav Arya <gauravarya272@gmail.com>
2023-01-30 21:54:25 +0800

    Rename triples test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
 
 @time begin if GROUP == "All"
-    @time @safetestset "Distributions" begin include("triples.jl") end
+    @time @safetestset "Triples" begin include("triples.jl") end
     @time @safetestset "Game of life" begin include("game_of_life.jl") end
     @time @safetestset "Random walk" begin include("random_walk.jl") end
     @time @safetestset "Resampling" begin include("resampling.jl") end

a13b523a7368f3badea3d05256901942fde67cec
Gaurav Arya <gauravarya272@gmail.com>
2023-01-30 21:27:00 +0800

    Bump number of samples on GOL test

--- test/game_of_life.jl
+++ test/game_of_life.jl
@@ -6,7 +6,7 @@ include("../tutorials/game_of_life/core.jl")
 
 @testset "AD and Finite Differences" begin
     p = 0.5
-    nsamples = 100_000
+    nsamples = 200_000
     samples_fd_clever = [GoLCore.fd_clever(p) for i in 1:nsamples]
     samples_st = [derivative_estimate(GoLCore.play, p) for i in 1:nsamples]
 

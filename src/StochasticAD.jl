aa0b05224dea3d866455f53db0f5ae4c7bcd9d4f
Gaurav Arya <gauravarya272@gmail.com>
2023-05-09 12:29:48 -0400

    Add smooth_triple

--- src/StochasticAD.jl
+++ src/StochasticAD.jl
@@ -2,7 +2,7 @@ module StochasticAD
 
 ### Public API
 
-export stochastic_triple, derivative_contribution, perturbations # For working with stochastic triples
+export stochastic_triple, derivative_contribution, perturbations, smooth_triple # For working with stochastic triples
 export derivative_estimate, StochasticModel, stochastic_gradient # Higher level functionality
 export new_weight # Particle resampling
 export PrunedFIsBackend, PrunedFIsAggressiveBackend, DictFIsBackend, SmoothedFIsBackend

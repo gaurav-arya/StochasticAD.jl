95ead5a65b90caaab48d3255671e1c28d0bce412
Gaurav Arya <gauravarya272@gmail.com>
2024-06-19 15:51:42 -0400

    Documenting algorithm interface and reverse mode

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,13 +1,75 @@
 abstract type AbstractStochasticADAlgorithm end
 
+"""
+    ForwardAlgorithm(backend::StochasticAD.AbstractFIsBackend) <: AbstractStochasticADAlgorithm
+    
+A differentiation algorithm relying on forward propagation of stochastic triples.
+
+The `backend` argument controls the algorithm used by the third component of the stochastic triples.
+
+!!! note 
+    The required computation time for forward-mode AD scales linearly with the number of 
+    parameters in `p` (but is unaffected by the number of parameters in `X(p)`).
+"""
 struct ForwardAlgorithm{B <: StochasticAD.AbstractFIsBackend} <: AbstractStochasticADAlgorithm
     backend::B
 end
 
+"""
+    EnzymeReverseAlgorithm(backend::StochasticAD.AbstractFIsBackend) <: AbstractStochasticADAlgorithm
+
+A differentiation algorithm relying on transposing the propagation of stochastic triples to
+produce a reverse-mode algorithm. The transposition is performed by [Enzyme.jl](https://github.com/EnzymeAD/Enzyme.jl),
+which must be loaded for the algorithm to run.
+
+Currently, only real- and vector-valued inputs are supported, and only real-valued outputs are supported.
+
+The `backend` argument controls the algorithm used by the third component of the stochastic triples.
+
+!!! warning
+    For the reverse-mode algorithm to yield correct results, the employed `backend` cannot use input-dependent pruning  
+    strategies. A suggested reverse-mode compatible backend is `PrunedFIsBackend(Val(:wins))`.
+"""
 struct EnzymeReverseAlgorithm{B <: StochasticAD.AbstractFIsBackend}
     backend::B
 end
 
-function StochasticAD.derivative_estimate(X, p, alg::ForwardAlgorithm; direction = nothing)
+function derivative_estimate(X, p, alg::ForwardAlgorithm; direction = nothing)
     return derivative_estimate(X, p; backend = alg.backend, direction)
 end
+
+@doc raw"""
+    derivative_estimate(X, p, alg::AbstractStochasticADAlgorithm = ForwardAlgorithm(PrunedFIsBackend()); direction=nothing)
+
+Compute an unbiased estimate of ``\frac{\mathrm{d}\mathbb{E}[X(p)]}{\mathrm{d}p}``, 
+the derivative of the expectation of the random function `X(p)` with respect to its input `p`.
+
+Both `p` and `X(p)` can be any object supported by [`Functors.jl`](https://fluxml.ai/Functors.jl/stable/),
+e.g. scalars or abstract arrays. 
+The output of `derivative_estimate` has the same outer structure as `p`, but with each
+scalar in `p` replaced by a derivative estimate of `X(p)` with respect to that entry.
+For example, if `X(p) <: AbstractMatrix` and `p <: Real`, then the output would be a matrix.
+
+The `alg` keyword argument specifies the [algorithm](public_api.md#Algorithms) used to compute the derivative estimate.
+For backward compatibility, an additional signature `derivative_estimate(X, p; backend, direction=nothing)`
+is supported, which uses `ForwardAlgorithm` by default with the supplied `backend.`
+
+When `direction` is provided, the output is only differentiated with respect to a perturbation
+of `p` in that direction.
+
+# Example
+```jldoctest
+julia> using Distributions, Random, StochasticAD; Random.seed!(4321);
+
+julia> derivative_estimate(rand ∘ Bernoulli, 0.5) # A random quantity that averages to the true derivative.
+2.0
+
+julia> derivative_estimate(x -> [rand(Bernoulli(x * i/4)) for i in 1:3], 0.5)
+3-element Vector{Float64}:
+ 0.2857142857142857
+ 0.6666666666666666
+ 0.0
+```
+"""
+
+derivative_estimate

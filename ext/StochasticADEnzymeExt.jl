4fb754d10719042fc43d32eb227db4ee6c0f210d
Gaurav Arya <gauravarya272@gmail.com>
2024-06-19 14:09:44 -0400

    Add EnzymeReverseAlgorithm, implemented in extension

---
+++ ext/StochasticADEnzymeExt.jl
@@ -0,0 +1,33 @@
+module StochasticADEnzymeExt
+
+using StochasticAD 
+using Enzyme
+
+function enzyme_target(u, X, p, backend)
+    # equivalent to derivative_estimate(X, p; backend, direction = u), but specialize to real output to make Enzyme happier
+    st = StochasticAD.stochastic_triple_direction(X, p, u; backend)
+    if !(StochasticAD.valtype(st) <: Real)
+        error("EnzymeReverseAlgorithm only supports real-valued outputs.")
+    end
+    return derivative_contribution(st)
+end
+
+function StochasticAD.derivative_estimate(X, p, alg::StochasticAD.EnzymeReverseAlgorithm; direction = nothing)
+    if !isnothing(direction)
+        error("EnzymeReverseAlgorithm does not support keyword argument `direction`")
+    end
+    if p isa AbstractVector
+        Δu = zeros(float(eltype(p)), length(p))
+        u = rand(StochasticAD.RNG, float(eltype(p)), length(p))
+        autodiff(Enzyme.Reverse, enzyme_target, Active, Duplicated(u, Δu), Const(X), Const(p), Const(alg.backend))
+        return Δu
+    elseif p isa Real
+        u = rand(StochasticAD.RNG, float(typeof(p)))
+        ((du, _, _, _),) = autodiff(Enzyme.Reverse, enzyme_target, Active, Active(u), Const(X), Const(p), Const(alg.backend))
+        return du
+    else
+        error("EnzymeReverseAlgorithm only supports p::Real or p::AbstractVector")
+    end
+end
+
+end
--- src/algorithms.jl
+++ src/algorithms.jl
@@ -4,6 +4,10 @@ struct ForwardAlgorithm{B <: StochasticAD.AbstractFIsBackend} <: AbstractStochas
     backend::B
 end
 
+struct EnzymeReverseAlgorithm{B <: StochasticAD.AbstractFIsBackend}
+    backend::B
+end
+
 function StochasticAD.derivative_estimate(X, p, alg::ForwardAlgorithm; direction = nothing)
     return derivative_estimate(X, p; backend = alg.backend, direction)
 end
